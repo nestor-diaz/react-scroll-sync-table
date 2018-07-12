2d70a21e77df7bddc62a6ae0e45ef85bfcbb0427
Nestor Diaz <nestor.loaiza@mulesoft.com>
2018-07-12 15:38:11 -0500

    - Add neutrino and its settings

---
+++ .neutrinorc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  use: [
+    'neutrino-preset-mozilla-frontend-infra/styleguide',
+    ['neutrino-preset-mozilla-frontend-infra/react-components', {
+      style: {
+        extract: false,
+      }
+    }],
+    (neutrino) => {
+      if (neutrino.options.command === 'styleguide:start') {
+        neutrino.config.module.rules.delete('lint');
+      }
+    },
+  ],
+};
